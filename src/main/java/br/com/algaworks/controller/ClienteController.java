c1d944093893d89d29bad158445f7768e111c935
Clemilson <klemilsondantas@gmail.com>
2020-11-26 23:26:50 -0300

    Integração com Flayway migration

--- src/main/java/br/com/algaworks/controller/ClienteController.java
+++ src/main/java/br/com/algaworks/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.algaworks.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,32 +12,13 @@ import br.com.algaworks.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return repositorio();
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
-	public static List<Cliente> repositorio() {
-
-		var c1 = new Cliente();
-		c1.setId(1L);
-		c1.setNome("Clemilson");
-		c1.setEmail("clemilson@gmail.com");
-		c1.setTelefone("99 99999-9999");
-
-		var c2 = new Cliente();
-		c2.setId(2L);
-		c2.setNome("Kamila");
-		c2.setEmail("kamila@gmail.com");
-		c2.setTelefone("99 88888-8888");
-
-		var c3 = new Cliente();
-		c3.setId(3L);
-		c3.setNome("Socorro");
-		c3.setEmail("socorro@gmail.com");
-		c3.setTelefone("99 77777-7777");
-
-		return Arrays.asList(c1, c2, c3);
-	}
 }
