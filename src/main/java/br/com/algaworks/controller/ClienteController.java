3f29f3f938b776aea1a854d4c5e503d20b95038c
Clemilson <klemilsondantas@gmail.com>
2020-11-26 22:30:57 -0300

    Adicionado conexão com banco

--- src/main/java/br/com/algaworks/controller/ClienteController.java
+++ src/main/java/br/com/algaworks/controller/ClienteController.java
@@ -15,21 +15,27 @@ public class ClienteController {
 	public List<Cliente> listar() {
 		return repositorio();
 	}
-	
+
 	public static List<Cliente> repositorio() {
-		
+
 		var c1 = new Cliente();
 		c1.setId(1L);
 		c1.setNome("Clemilson");
 		c1.setEmail("clemilson@gmail.com");
 		c1.setTelefone("99 99999-9999");
-		
+
 		var c2 = new Cliente();
 		c2.setId(2L);
 		c2.setNome("Kamila");
 		c2.setEmail("kamila@gmail.com");
 		c2.setTelefone("99 88888-8888");
-		
-		return Arrays.asList(c1, c2);
+
+		var c3 = new Cliente();
+		c3.setId(3L);
+		c3.setNome("Socorro");
+		c3.setEmail("socorro@gmail.com");
+		c3.setTelefone("99 77777-7777");
+
+		return Arrays.asList(c1, c2, c3);
 	}
 }
